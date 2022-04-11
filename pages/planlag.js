e1ca2ca8379b11d0ffccba6830ab6dedc5d3a8c9
greenplank <info@greenplank.eu>
2022-04-11 13:20:27 +0500

    newDDK

--- pages/planlag.js
+++ pages/planlag.js
@@ -2,7 +2,7 @@ import React from "react";
 import Layout from "../components/layout";
 import HeaderTwo from "../components/header/header-two";
 import StickyHeader from "../components/header/sticky-header";
-import PageHeader from "../components/page-header";
+import PageHeader from "../components/page-header-planner";
 import PlaneraSection from "../components/planeraSection";
 import Footer from "../components/footer";
 import Head from "next/head";
@@ -17,7 +17,7 @@ const Planlag = () => {
       </Head>
       <Layout pageTitle="Planlæg - GreenPlank.dk">
         <HeaderTwo />
-        <PageHeader title="Planlæg" crumbTitle="planlæg" />
+        <PageHeader title="" crumbTitle="planlæg" />
         <StickyHeader />
         <PlaneraSection />
         <Footer />
